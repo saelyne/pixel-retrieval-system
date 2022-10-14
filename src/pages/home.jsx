a88a561776dd8c22f46e94232b5c325c893bd91b
saelyne <saelyne@kaist.ac.kr>
2022-10-14 15:39:24 +0900

    instruction added

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,11 +7,19 @@ import { Form } from "react-bootstrap";
 import Button from "@mui/material/Button";
 
 import { imageInfoCollection, answerInfoCollection } from "../image";
-import main from "../static/img/main.png"
-import selected from "../static/img/selected.png"
-import labeled from "../static/img/labeled.png"
+import main from "../static/img/main.png";
+import selected from "../static/img/selected.png";
+import labeled from "../static/img/labeled.png";
 
-const Home = ({ setUserID, setModeID, setGroupImageInfo, setGroupAnswerInfo, MAX_SELECTED_COUNT, NUM_IMAGES, NUM_QUESTIONS }) => {
+const Home = ({
+  setUserID,
+  setModeID,
+  setGroupImageInfo,
+  setGroupAnswerInfo,
+  MAX_SELECTED_COUNT,
+  NUM_IMAGES,
+  NUM_QUESTIONS,
+}) => {
   const idRef = React.createRef();
   const navigate = useNavigate();
   const { id } = useParams();
@@ -29,36 +37,70 @@ const Home = ({ setUserID, setModeID, setGroupImageInfo, setGroupAnswerInfo, MAX
 
   return (
     <>
-      <Header questionID = {-1} NUM_QUESTIONS = {NUM_QUESTIONS} />
+      <Header questionID={-1} NUM_QUESTIONS={NUM_QUESTIONS} />
       <div className="home-window">
         <div className="tutorial-window">
-          <div>In this study, you will go through {NUM_QUESTIONS-2} questions that ask you to choose images.</div>
-          <div>For each question, there is a <span style={{color:"Purple"}}>Target Image</span> (left), and {NUM_IMAGES} <span style={{color:"Purple"}}>Candidate Images</span> (right).</div>
-          <div>Among Candidate Images, you need to <span style={{color:"Brown"}}>choose {MAX_SELECTED_COUNT} images that you think contain the object in the Target Image</span>. </div>
+          <div>
+            In this study, you will go through {NUM_QUESTIONS - 2} questions
+            that ask you to choose images.
+          </div>
+          <div>
+            For each question, there is a{" "}
+            <span style={{ color: "Purple" }}>Target Image</span> (left), and{" "}
+            {NUM_IMAGES}{" "}
+            <span style={{ color: "Purple" }}>Candidate Images</span> (right).
+          </div>
+          <div>
+            Among Candidate Images, you need to{" "}
+            <span style={{ color: "Brown" }}>
+              choose {MAX_SELECTED_COUNT} images that you think contain the
+              object in the Target Image
+            </span>
+            .{" "}
+          </div>
           <img src={main}></img>
-          <div>Sometimes, there will be hints that indicate a certain part of the image that MIGHT contain the Target Image as shown below.</div>
-          <div>For this type of questions, <span style={{color:"Brown"}}> you only have to compare the highlighted parts</span>, which can help you complete the task faster. </div>
+          <div>
+            Sometimes, there will be hints that indicate a certain part of the
+            image that MIGHT contain the Target Image as shown below.
+          </div>
+          <div>
+            For this type of questions,{" "}
+            <span style={{ color: "Brown" }}>
+              {" "}
+              you only have to compare the highlighted parts
+            </span>
+            , which can help you complete the task faster.{" "}
+          </div>
           <img src={labeled}></img>
           <div className="warning">
-            * Do not refresh the page while completing the task. <br />
-            * Data we collect: user ID and time taken to complete each question. <br />
-            * We collect your ID to verify you on Prolific. The collected IDs will be discarded after the study.
-            <div style={{marginTop: "15px", marginBottom:"15px"}}>If you agree with the above terms, please enter your <span style={{fontWeight: 800}}>unique Prolific ID</span> to start the task.</div>
+            * Do not refresh the page or go back to a previous page while
+            completing the task. <br />
+            * Data we collect: user ID and time taken to complete each question.{" "}
+            <br />* We collect your ID to verify you on Prolific. The collected
+            IDs will be discarded after the study.
+            <div style={{ marginTop: "15px", marginBottom: "15px" }}>
+              If you agree with the above terms, please enter your{" "}
+              <span style={{ fontWeight: 800 }}>unique Prolific ID</span> to
+              start the task.
+            </div>
           </div>
         </div>
         <div className="input-window">
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            {/* <Form.Label>To start the task, please enter your <span style={{fontWeight: 800}}>unique Prolific ID.</span></Form.Label> */}
-            <Form.Control placeholder="Enter your unique Prolific ID" ref={idRef} />
-            <Form.Text className="text-muted">
-              {/* We'll never share your email with anyone else. */}
-            </Form.Text>
-          </Form.Group>
-          <Button variant="contained" onClick={() => onClickSubmit()}>
-            Submit
-          </Button>
-        </Form>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              {/* <Form.Label>To start the task, please enter your <span style={{fontWeight: 800}}>unique Prolific ID.</span></Form.Label> */}
+              <Form.Control
+                placeholder="Enter your unique Prolific ID"
+                ref={idRef}
+              />
+              <Form.Text className="text-muted">
+                {/* We'll never share your email with anyone else. */}
+              </Form.Text>
+            </Form.Group>
+            <Button variant="contained" onClick={() => onClickSubmit()}>
+              Submit
+            </Button>
+          </Form>
         </div>
       </div>
     </>
